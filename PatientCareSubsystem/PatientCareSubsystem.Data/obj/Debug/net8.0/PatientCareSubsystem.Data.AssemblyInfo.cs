494971053cdefa1511006ed8f34f6e958a1b7cf1
kxnnxthmxlgxs <73911764+kxnnxthmxlgxs@users.noreply.github.com>
2024-09-30 21:03:19 +0200

    made changes to login

--- PatientCareSubsystem/PatientCareSubsystem.Data/obj/Debug/net8.0/PatientCareSubsystem.Data.AssemblyInfo.cs
+++ PatientCareSubsystem/PatientCareSubsystem.Data/obj/Debug/net8.0/PatientCareSubsystem.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientCareSubsystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784eaffe39c99dde4f83a77d00482642a8dce751")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientCareSubsystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientCareSubsystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
