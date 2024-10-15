51470e5df1cf6599e9c0c5a6958cd862095b083b
kxnnxthmxlgxs <73911764+kxnnxthmxlgxs@users.noreply.github.com>
2024-10-15 14:47:40 +0200

    made changes to nurse dashboard

--- PatientCareSubsystem/PatientCareSubsystem.Data/obj/Debug/net8.0/PatientCareSubsystem.Data.AssemblyInfo.cs
+++ PatientCareSubsystem/PatientCareSubsystem.Data/obj/Debug/net8.0/PatientCareSubsystem.Data.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatientCareSubsystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784eaffe39c99dde4f83a77d00482642a8dce751")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e812d88b5e3b124b82c5133aad4382ea02f62f")]
->>>>>>> KennethSubSystem
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7327f0f50aa80a9769827eef59e976607267a5b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatientCareSubsystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatientCareSubsystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
